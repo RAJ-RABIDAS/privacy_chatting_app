827d282d4775e6430843c32f5a2593cae79e6fc4
Rajrabidas <rajonfiver@gmail.com>
2022-06-19 13:38:37 +0530

    Updates

---
+++ precache-manifest.4afe323ea43ab6e126b4b78a17169a87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b17ac04bceb77f4358214b090aa95bc2",
+    "url": "/privacy_chatting_app/index.html"
+  },
+  {
+    "revision": "4fe8cf4a4678e52f9855",
+    "url": "/privacy_chatting_app/static/css/main.9cb7495d.chunk.css"
+  },
+  {
+    "revision": "2292ae6d8d6fe2a5c887",
+    "url": "/privacy_chatting_app/static/js/2.c6fc335b.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/privacy_chatting_app/static/js/2.c6fc335b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fe8cf4a4678e52f9855",
+    "url": "/privacy_chatting_app/static/js/main.f6067450.chunk.js"
+  },
+  {
+    "revision": "059bb9a4eb2b6b16b313",
+    "url": "/privacy_chatting_app/static/js/runtime-main.e0dbde07.js"
+  }
+]);
