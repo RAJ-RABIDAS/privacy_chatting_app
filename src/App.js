a93b532e411e91ff16f29de102f600059637de51
Rajrabidas <rajonfiver@gmail.com>
2022-06-19 13:57:24 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -44,21 +44,21 @@ function App() {
   );
 }
 
-function SignIn() {
+// function SignIn() {
 
-  const signInWithGoogle = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider);
-  }
+//   const signInWithGoogle = () => {
+//     const provider = new firebase.auth.GoogleAuthProvider();
+//     auth.signInWithPopup(provider);
+//   }
 
-  return (
-    <>
-      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
-      <p>Do not violate the community guidelines or you will be banned for life!</p>
-    </>
-  )
+  // return (
+  //   <>
+  //     <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+  //     <p>Do not violate the community guidelines or you will be banned for life!</p>
+  //   </>
+  // )
 
-}
+// }
 
 function SignOut() {
   return auth.currentUser && (
